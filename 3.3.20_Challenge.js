a45a8f9fef8498a552f7f4eba34e27d70ee6c05a
AR-Lilypad <angie-for-seekers@msn.com>
2020-03-10 21:30:51 -0400

    initial files upload

---
+++ 3.3.20_Challenge.js
@@ -0,0 +1,8 @@
+// Coding challenges:
+// Create a new github repo named coding-challenges (or whatever suitable name you would like) and a folder with the same name on your computer;
+// Connect your local folder with the github repo (you need to run git init, then git remote add origin)
+// Create two branches for each challenge below; once you are done with the code push to github and merge into master. (you will have two branches merged into master);
+// 1. Write a function that takes three arguments - all numbers - adds them together, split the result in 3 and displays the result on the console.
+
+
+
